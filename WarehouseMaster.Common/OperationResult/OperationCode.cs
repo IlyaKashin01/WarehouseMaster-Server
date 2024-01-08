b36b9eea82e62a4628987520aeea15cb6aba492d
IlyaKashin01 <sergeich33rus2001@gmail.com>
2024-01-08 18:01:10 +0300

    Добавьте файлы проекта.

---
+++ WarehouseMaster.Common/OperationResult/OperationCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseMaster.Common.OperationResult
+{
+    public enum OperationCode : short
+    {
+        Ok = 1,
+        UnhandledError = 2,
+        Error = 3,
+        ValidationError = 4,
+        EntityWasNotFound = 5,
+        AlreadyExists = 6,
+        Unauthorized = 7,
+        Forbidden = 8
+    }
+}
---
+++ WarehouseMaster.Common/OperationResult/OperationResult.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseMaster.Common.OperationResult
+{
+    public class OperationResult
+    {
+        public bool Success => ErrorCode == 0;
+        public OperationCode ErrorCode { get; set; }
+        public string? Message { get; set; }
+        public string? StackTrace { get; set; }
+
+        public static OperationResult OK => new OperationResult();
+        public static OperationResult Fail(OperationCode errorCode, string? message = null, string? stackTrace = null)
+            => new OperationResult(errorCode, message, stackTrace);
+
+        protected OperationResult()
+        {
+
+        }
+
+        public OperationResult(OperationCode operationCode, string? message = null, string? stackTrace = null)
+        {
+            ErrorCode = operationCode;
+            Message = message;
+            StackTrace = stackTrace;
+        }
+    }
+
+    public class OperationResult<T> : OperationResult
+    {
+        public T? Result { get; set; }
+
+        public static new OperationResult<T> Fail(OperationCode errorCode, string? message = null, string? stackTrace = null)
+            => new OperationResult<T>(errorCode, message, stackTrace);
+
+        public OperationResult()
+        {
+
+        }
+
+        public OperationResult(T result)
+        {
+            Result = result;
+        }
+
+        public OperationResult(OperationCode errorCode, string? message = null, string? stackTrace = null) : base(errorCode, message, stackTrace)
+        {
+
+        }
+    }
+}
